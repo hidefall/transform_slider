513c6a1f0c5de5130217833d9cc3ed6d032ac81c
hidefall <hidefall@gmail.com>
2017-11-06 11:23:08 +0200

    updates

--- js/slider.js
+++ js/slider.js
@@ -5,17 +5,18 @@
     $('.slider').slick({
       slidesToShow: 1,
       pauseOnFocus: true,
-      autoplay: false,
+      autoplay: true,
       autoPlaySpeed: 1001,
       pauseOnHover: true,
       infinite: true,
+      arrows: true,
       //centerMode: true,
       adaptiveHeight: true,
-      // draggable: false,
+      draggable: false,
 
     });
   });
-  
+
 function idea(){
         var wh = $(window).height();
         var bw = parseInt($('.slide_title_border').css('border-left-width'));
@@ -32,17 +33,5 @@ function idea(){
     $(window).resize(function(){
       idea()
     });
-
-  // $(document).ready(function(){
-  // var pos = $('.site_title_border').position()
-  // var dim = $('.site_title_border').height()
-  // $('.slider_title').offset({
-  //   top: pos.top + dim/2 - $('.slider_title').height()/2, 
-  //   left: pos.left
-  // });
-  // });
-  // var pos = $('.trap').position()
-  // var dim = $('.trap').height()
-  // $('.text').offset({top: pos.top + dim/2 - $('.text').height()/2, left: pos.left});
 
  })(jQuery);
